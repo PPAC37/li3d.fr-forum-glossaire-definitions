5afd74a5c8d2a70f533edc75d005d5591868d4ee
q6 <q6@q6-pc>
2023-01-04 01:29:31 +0100

    base d'un projet Java Maeven et fichiers .md déplacé de li3d.fr-forum-glossaire

---
+++ src/main/java/com/ppac/li3d/fr/forum/glossaire/definitions/Li3dFrForumGlossaireDefinitions.java
@@ -0,0 +1,15 @@
+/*
+ */
+
+package com.ppac.li3d.fr.forum.glossaire.definitions;
+
+/**
+ *
+ * @author q6
+ */
+public class Li3dFrForumGlossaireDefinitions {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
